d8442a1ad67dced1ddeba0414ad120e377e06663
Fabrice Brito <fabrice.brito@terradue.com>
2024-12-16 15:19:59 +0100

    expands init/dispose scripts/reduces etc config

--- example/pod-procure/dispose-dask.py
+++ example/pod-procure/dispose-dask.py
@@ -6,10 +6,12 @@ from dask_gateway import Gateway
 # read the arguments containing the target file path for the Dask cluster name
 parser = argparse.ArgumentParser()
 parser.add_argument("--source", type=str, required=True)
+parser.add_argument("--gateway-url", type=str, required=True)
 parser.add_argument("--signal", type=str, required=True)
 args = parser.parse_args()
 
 source = args.source
+gateway_url = args.gateway_url
 signal = args.signal
 
 logger.info(f"Sidecar: Waiting for completion signal ({signal}) from main container...")
@@ -25,7 +27,7 @@ logger.info("Sidecar: Completion signal received. Shutting down Dask cluster..."
 with open(source, "r") as f:
     cluster_name = f.read().strip()
 
-gateway = Gateway()
+gateway = Gateway(gateway_url)
 cluster = gateway.connect(cluster_name)
 logger.info(f"Sidecar: Connected to Dask cluster: {cluster_name}")
 cluster.shutdown()
--- example/pod-procure/init-dask.py
+++ example/pod-procure/init-dask.py
@@ -9,28 +9,49 @@ from dask_gateway import Gateway
 # read the arguments containing the target file path for the Dask cluster name
 parser = argparse.ArgumentParser()
 parser.add_argument("--target", type=str, required=True)
+parser.add_argument("--gateway-url", type=str, required=True)
+parser.add_argument("--image", type=str, required=True)
+parser.add_argument("--worker-cores", type=float, required=True, default=0.5)
+parser.add_argument("--worker-cores-limit", type=float, required=True, default=1)
+parser.add_argument("--worker-memory", type=int, required=True, default=2)
+parser.add_argument("--max-cores", type=int, required=True, default=5)
+parser.add_argument("--max-ram", type=int, required=True, default=16)
 args = parser.parse_args()
 
 target = args.target
+gateway_url = args.gateway_url
+image = args.image
+worker_cores = args.worker_cores
+worker_cores_limit = args.worker_cores_limit
+worker_memory = args.worker_memory
+max_cores = args.max_cores
+max_ram = args.max_ram
 
 logger.info(f"Creating Dask cluster and saving the name to {target}")
 
-gateway = Gateway()
+gateway = Gateway(gateway_url)
 
-cluster = gateway.new_cluster(shutdown_on_close=False)
+cluster_options = gateway.cluster_options()
+
+cluster_options['image'] = image
+cluster_options['worker_cores'] = worker_cores
+cluster_options['worker_cores_limit'] = worker_cores_limit
+cluster_options['worker_memory'] = f"{worker_memory} G"
+
+cluster = gateway.new_cluster(cluster_options, shutdown_on_close=False)
 
 # resource requirements
-worker_cores = 0.5
-worker_cores_limit = 1 # would come from DaskGateway.Requirement.ResourceRequirement.worker_cores_limit (or worker_cores)
-worker_memory = 2 # would come from DaskGateway.Requirement.ResourceRequirement.worker_memory
+#worker_cores = 0.5
+#worker_cores_limit = 1 # would come from DaskGateway.Requirement.ResourceRequirement.worker_cores_limit (or worker_cores)
+#worker_memory = 2 # would come from DaskGateway.Requirement.ResourceRequirement.worker_memory
 logger.info(f"Resource requirements: {worker_cores} cores, {worker_memory} GB RAM")
 
 # scale cluster
-max_cores = 5 # would come from DaskGateway.Requirement.ResourceRequirement.max_cores
-max_ram = 16  # would come from DaskGateway.Requirement.ResourceRequirement.max_ram
+# max_cores = 5 # would come from DaskGateway.Requirement.ResourceRequirement.max_cores
+# max_ram = 16  # would come from DaskGateway.Requirement.ResourceRequirement.max_ram
 logger.info(f"Resource limits: {max_cores} cores, {max_ram} GB RAM")
 
-workers = min(max_cores // worker_cores_limit, max_ram // worker_memory)
+workers = int(min(max_cores // worker_cores_limit, max_ram // worker_memory))
 logger.info(f"Scaling cluster to {workers} workers")
 cluster.scale(workers)
 
